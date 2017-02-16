ce4ebea61e396b73b900b906fe014f0ebae40a1c
Steven Ng <steven.ng@temple.edu>
2017-02-16 16:31:34 -0500

    Clean up logic and use default args

--- lib/harvest_csv.rb
+++ lib/harvest_csv.rb
@@ -3,30 +3,34 @@ require 'csv'
 require 'rsolr'
 require 'pry'
 require 'yaml'
+require 'securerandom'
 
 module HarvestCSV
   def self.csv_to_solr(csv_hash, schema_map)
     document = Hash.new
+    document["id"] = SecureRandom.uuid
     csv_hash.each { |key, value|
-      solr_fields = schema_map[key]
-      solr_fields.each {|solr_field|
-        document[solr_field] = value
-      }
+      if (schema_map.has_key?(key))
+        solr_fields = schema_map[key]
+        solr_fields.each {|solr_field|
+          document[solr_field] = value
+        }
+      end
     }
     document
   end
 
   def self.harvest(csv_source,
-                   map_source,
-                   solr_endpoint = 'http://localhost:8983/solr/blacklight-core')
+                   map_source = './solr_map.yml',
+                   solr_endpoint = 'http://localhost:8983/solr/blacklight-core' )
     schema_map = YAML.load_file(map_source)
+    solr = RSolr.connect url: solr_endpoint
     CSV.open(csv_source, headers: true) do |csv|
-      solr = RSolr.connect url: solr_endpoint
       csv.each do |row|
         document = csv_to_solr(row.to_h, schema_map)
-        solr.add document
-        solr.commit
+        solr.add(document)
       end
     end
+    solr.commit
   end
 end
